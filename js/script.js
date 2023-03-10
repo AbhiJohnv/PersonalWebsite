e319952b433b46797de0022011bdcc1c5fee6387
AbhiJohnv <122664254+AbhiJohnv@users.noreply.github.com>
2023-03-10 08:30:08 +0530

    Update the d of design to Design

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@
 
 var typed = new Typed(".multiple-text",
     {
-        strings: ["Web designer", "Web Developer", "Fullstack Developer"],
+        strings: ["Web Designer", "Web Developer", "Fullstack Developer"],
         typeSpeed: 100,
         backSpeed: 100,
         backDelay: 1000,
